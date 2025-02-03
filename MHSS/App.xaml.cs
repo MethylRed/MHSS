74a33a5de0d7f2c7d5702eeedd1a6cdaa32a403e
ryosuke.nakajima <ryosuke.nakajima@OA1050>
2025-02-03 14:47:05 +0900

    プロジェクト ファイルを追加します。

---
+++ MHSS/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MHSS.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace MHSS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ MHSS/ViewModels/ViewModelBase.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Reactive.Disposables;
+
+namespace MHSS.ViewModels
+{
+    public abstract class ViewModelBase : BindableBase, IDisposable, IDestructible
+    {
+        /// ReactiveProperty一括破棄用フィールド
+        protected CompositeDisposable _disposables = new();
+        /// 二重破棄防止用フラグ
+        private bool _disposed;
+        /// リソースの破棄処理
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        /// リソースの破棄処理の実態
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) { return; }
+            if (disposing)
+            {
+                /* マネージリソースの破棄処理 */
+                _disposables?.Dispose();
+            }
+            try { /* アンマネージドリソースの破棄処理 */ }
+            catch { }
+            _disposed = true;
+        }
+        /// ファイナライザ
+        ~ViewModelBase() => Dispose(false);
+        /// ViewModel破棄
+        public void Destroy() => Dispose();
+    }
+}
